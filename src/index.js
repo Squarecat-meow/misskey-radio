431624912e9387988f4da8bb72610d220d5404f8
squarecat-meow <kw7551@gmail.com>
2023-11-14 10:16:35 +0900

    edit for routing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <CookiesProvider>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
