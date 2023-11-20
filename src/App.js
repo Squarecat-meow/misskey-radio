fc039507717e35747bc26cc3d30953b7459d90a1
squarecat-meow <kw7551@gmail.com>
2023-11-21 08:24:10 +0900

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { Routes, Route, useLocation } from "react-router-dom";
 import ModalWindow from "./components/ModalWindow";
